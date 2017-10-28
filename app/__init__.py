7a6eb2ca625677a0bfb46ad8aa3f49f91e2988f8
Raymond Rupiah <rupiray@yahoo.com>
2017-10-28 23:32:05 +0100

    new file updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 # app/__init__.py
 
+import os
+
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -8,12 +10,24 @@ from flask_bootstrap import Bootstrap
 
 from config import app_config
 
+
 # db variable initialization
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 
 def create_app(config_name):
+    if os.getenv('FLASK_CONFIG') == "production":
+        app = Flask(__name__)
+        app.config.update(
+            SECRET_KEY=os.getenv('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+        )
+    else:
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(app_config[config_name])
+        app.config.from_pyfile('config.py')
+    
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
